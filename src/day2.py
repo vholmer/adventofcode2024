fdc9c0a0ad40f5d1b6fe6a4bfbe72cc700a972d1
Viktor Holmér <viktorholmer@gmail.com>
2024-12-02 23:01:21 +0100

    Ruff

--- src/day2.py
+++ src/day2.py
@@ -1,6 +1,6 @@
 def get_matrix() -> list[list[int]]:
     matrix = []
-    
+
     with open("data/2/data.txt") as f:
         for line in f:
             ints = [int(x) for x in line.strip().split(" ")]
@@ -8,12 +8,13 @@ def get_matrix() -> list[list[int]]:
 
     return matrix
 
+
 def is_row_safe(row: list[int]) -> int:
-    is_safe = row == sorted(row) or row == sorted(row, reverse = True)
+    is_safe = row == sorted(row) or row == sorted(row, reverse=True)
 
     if not is_safe:
         return 0
-        
+
     for i in range(len(row) - 1):
         cur = row[i]
         nex = row[i + 1]
@@ -24,6 +25,7 @@ def is_row_safe(row: list[int]) -> int:
             return 0
 
     return 1
+
 
 def solve():
     m = get_matrix()
@@ -36,16 +38,16 @@ def solve():
     print(f"2A: {num_safe}")
 
     m = get_matrix()
-    
+
     num_safe = 0
 
     for row in m:
         # First check row regularly
         is_safe = bool(is_row_safe(row))
-            
+
         # Then, attempt to remove each element one by one
         for i in range(len(row)):
-            new_row = row[:i] + row[i+1:]
+            new_row = row[:i] + row[i + 1 :]
 
             is_safe = is_safe or bool(is_row_safe(new_row))
 
@@ -53,5 +55,3 @@ def solve():
             num_safe += 1
 
     print(f"2B: {num_safe}")
-
-            
