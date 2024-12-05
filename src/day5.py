819fde251a7be31f8ccba79a00dea8acedc5981a
Viktor Holmér <viktorholmer@gmail.com>
2024-12-05 21:52:10 +0100

    Solved day 5

--- src/day5.py
+++ src/day5.py
@@ -1,4 +1,4 @@
-def get_rules_and_updates() -> tuple[dict[int, int], list[list[int]]]:
+def get_rules_and_updates() -> tuple[dict[int, set[int]], list[list[int]]]:
     rules = {}
     updates = []
     
@@ -19,8 +19,9 @@ def get_rules_and_updates() -> tuple[dict[int, int], list[list[int]]]:
 
     return rules, updates
 
-def get_ordered_updates_idx(rules: dict[int, int], updates: list[list[int]]) -> list[int]:
+def get_updates_idx(rules: dict[int, int], updates: list[list[int]], return_ordered: bool = True) -> list[int]:
     ordered_updates = []
+    unordered_updates = []
         
     for i, update in enumerate(updates):
         #if i == 4:
@@ -42,10 +43,15 @@ def get_ordered_updates_idx(rules: dict[int, int], updates: list[list[int]]) -> 
             
         if update_ordered:
             ordered_updates.append(i)
+        else:
+            unordered_updates.append(i)
 
-    return ordered_updates
+    if return_ordered:
+        return ordered_updates
+    else:
+        return unordered_updates
 
-def get_middle_sum(updates: list[list[int]], ordered_updates_idx: list[int]) -> int:
+def get_middle_sum_idx(updates: list[list[int]], ordered_updates_idx: list[int]) -> int:
     middle_sum = 0
 
     for idx in ordered_updates_idx:
@@ -57,11 +63,51 @@ def get_middle_sum(updates: list[list[int]], ordered_updates_idx: list[int]) -> 
 
     return middle_sum
 
+def get_middle_sum(updates: list[list[int]]) -> int:
+    middle_sum = 0
+
+    for i in range(len(updates)):
+        update = updates[i]
+
+        middle_idx = len(update) // 2
+
+        middle_sum += update[middle_idx]
+
+    return middle_sum
+
+def sort_update(update: list[int], rules: dict[int, set[int]]) -> list[int]:
+    sorted_update = []
+    offset = -1000
+    for page in update:
+        if page not in rules:
+            sorted_update.append((offset, page))
+            continue
+
+        rule = rules[page]
+
+        n = sum([1 for otherpage in update if page != otherpage and otherpage in rule])
+
+        sorted_update.append((n, page))
+
+    sorted_update.sort(key = lambda x: x[0], reverse = True)
+
+    return [x[1] for x in sorted_update]
+
 def solve() -> None:
     rules, updates = get_rules_and_updates()
 
-    ordered_updates_idx = get_ordered_updates_idx(rules, updates)
+    ordered_updates_idx = get_updates_idx(rules, updates)
 
-    n = get_middle_sum(updates, ordered_updates_idx)
+    n = get_middle_sum_idx(updates, ordered_updates_idx)
 
     print(f"5A: {n}")
+
+    unordered_updates_idx = get_updates_idx(rules, updates, return_ordered = False)
+
+    fixed_updates = []
+    for idx in unordered_updates_idx:
+        fixed_updates.append(sort_update(updates[idx], rules))
+
+    n = get_middle_sum(fixed_updates)
+
+    print(f"5B: {n}")
